2fdce09965e7dd64ac91bf0e496600609ef50a6e
6em-dev <rimit58872@gmail.com>
2026-02-04 18:18:56 +0100

    Initial commit

---
+++ src/client/java/com/lampsutils/LampsUtilsClient.java
@@ -0,0 +1,12 @@
+package com.lampsutils;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.minecraft.client.DeltaTracker;
+import net.minecraft.client.gui.GuiGraphics;
+
+public class LampsUtilsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+	}
+}
---
+++ src/client/java/com/lampsutils/LampsUtilsDataGenerator.java
@@ -0,0 +1,11 @@
+package com.lampsutils;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LampsUtilsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+	}
+}
